91958a2c2dcf0991cb2f6c09bdbbac1aa8afb103
liquid <al20760619@ite.edu.mx>
2024-05-09 00:30:36 -0700

    first commit

---
+++ init.lua
@@ -0,0 +1,82 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' }}
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+    ensure_installed = {"lua", "c", "haskell", "javascript", "java"},
+    highlight = { enable = true },
+    indent = { enable = true },
+})
+
+require('lualine').setup {
+  options = {
+    icons_enabled = false,
+    theme = 'gruvbox',
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = false,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+
+require('lualine').setup()
+
+require("gruvbox").setup({
+  contrast = "soft",
+    --palette_overrides = {
+      --dark
+  --}
+})
+vim.cmd.colorscheme "gruvbox"
